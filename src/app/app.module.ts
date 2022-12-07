1bb72744de2f7c9c5d6353cde1639f3147c53be2
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2022-12-07 16:47:09 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BookingsComponent } from './bookings/bookings.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookingsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/bookings/bookings.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookings',
+  templateUrl: './bookings.component.html',
+  styleUrls: ['./bookings.component.css']
+})
+export class BookingsComponent {
+
+  constructor() { }
+
+  booking = 'Jannick';
+
+  ngOnInit(): void {
+
+  }
+
+}
