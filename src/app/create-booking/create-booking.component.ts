f95e35138ffd97d91f9b003206cfbb817ffc4cf2
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2022-12-09 18:49:12 +0100

    add form to create booking

--- src/app/create-booking/create-booking.component.ts
+++ src/app/create-booking/create-booking.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import { Booking } from '../booking';
+import { Bookings } from '../mock-bookings';
 @Component({
   selector: 'app-create-booking',
   templateUrl: './create-booking.component.html',
   styleUrls: ['./create-booking.component.css']
 })
+
 export class CreateBookingComponent {
+
+  constructor() {}
+
+  booking: Booking = {
+    id: 100,
+    name: 'your Name',
+    roomNumber: 100,
+    startDate: new Date(),
+    endDate: new Date()
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }
